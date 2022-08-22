1ac73e17caf3ab7496a29ed9139dd05c19615d9c
Madis <kalashnikov.madis@gmail.com>
2022-08-22 11:56:58 +0300

    Add color change on mouseover Add clear button to change all boxes backgrounds.

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const wrapper = document.querySelector(".boxes-wrapper")
-
 const multipleDivs = (num) => {
 	for (let i = 1; i <= num * num; i++) {
 		const div = document.createElement("div")
-		div.innerHTML = "a"
 		div.classList.add("box")
 		div.style.width = `${600 / num}px`
 		div.style.height = `${600 / num}px`
 		wrapper.appendChild(div)
 	}
 }
-multipleDivs(6)
+multipleDivs(16)
+const divs = document.querySelectorAll('.box')
+const changeColor = (e) => {
+	const target = e.target
+	if (target.classList.contains("black")) return;
+	target.classList.add("black")
+}
+divs.forEach(div => {
+	div.addEventListener("mouseover", changeColor)
+})
+
+const clearGrid = () => {
+	divs.forEach(div => {
+		div.classList.remove("black")
+	})
+}
+const clearBtn = document.querySelector("#clear")
+clearBtn.addEventListener("click", clearGrid)
