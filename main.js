2bd1f9515d7cd5da7f6ba10ed7c35d19bc5995f0
Madis <kalashnikov.madis@gmail.com>
2022-08-22 11:11:21 +0300

    Create function to make a * a grid of divs Add classes and markup for divs.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const wrapper = document.querySelector(".boxes-wrapper")
+
+const multipleDivs = (num) => {
+	for (let i = 1; i <= num * num; i++) {
+		const div = document.createElement("div")
+		div.innerHTML = "a"
+		div.classList.add("box")
+		wrapper.appendChild(div)
+	}
+}
+multipleDivs(16)
